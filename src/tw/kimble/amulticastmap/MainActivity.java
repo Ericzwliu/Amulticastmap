63da52625c71d22e94a173521fe4edb753f35f8e
ERIC.ZW.LIU <ERIC.ZW.LIU@TWTP2NB0014.delta.corp>
2015-06-21 12:10:08 +0800

    Second map already to test

---
+++ src/tw/kimble/amulticastmap/MainActivity.java
@@ -0,0 +1,63 @@
+package tw.kimble.amulticastmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.amulticastmap.R;
+import com.google.android.m4b.maps.CameraUpdateFactory;
+import com.google.android.m4b.maps.GoogleMap;
+import com.google.android.m4b.maps.MapFragment;
+import com.google.android.m4b.maps.OnMapReadyCallback;
+import com.google.android.m4b.maps.model.LatLng;
+import com.google.android.m4b.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity implements OnMapReadyCallback{
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		
+		mapFragment.getMapAsync(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap map) {
+		// TODO Auto-generated method stub
+		
+		LatLng sydney = new LatLng(-33.867, 151.206);
+		
+		map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+	}
+}
